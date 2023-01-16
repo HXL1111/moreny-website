fe6c4ed4ccf995463a9de036ca1099d6e4ba8266
HXL1111 <12226268+Hxl2521@user.noreply.gitee.com>
2023-01-16 17:01:44 +0800

    deploy

---
+++ precache-manifest.c0440ff15b1538d5a858b47eb345b01b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf90e9fdbe7d9b3dc5ff",
+    "url": "/moreny-website/css/app.6a0f01ca.css"
+  },
+  {
+    "revision": "1b1af6aaa75cd448983e",
+    "url": "/moreny-website/css/chunk-vendors.f2a4295a.css"
+  },
+  {
+    "revision": "7a934e7af7a5d2cae4cc7912b9269337",
+    "url": "/moreny-website/index.html"
+  },
+  {
+    "revision": "bf90e9fdbe7d9b3dc5ff",
+    "url": "/moreny-website/js/app.f9cee238.js"
+  },
+  {
+    "revision": "1b1af6aaa75cd448983e",
+    "url": "/moreny-website/js/chunk-vendors.6dc047cb.js"
+  },
+  {
+    "revision": "91cfeafc8fe2dc57fc1b6c08ae9cbc98",
+    "url": "/moreny-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moreny-website/robots.txt"
+  }
+]);
